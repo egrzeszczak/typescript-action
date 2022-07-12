2d38f2869b22019d560a1e4fbf5ca3767dd44308
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-07-12 15:34:03 +0200

    action: display context of @actions/github.context

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World')
+import { notice, setFailed } from "@actions/core";
+import { context } from "@actions/github";
+
+(
+    async () => {
+        try {
+            notice("Displaying context")
+            console.log(context)
+        } catch (error: any) {
+            setFailed(error.message)
+        }
+    }
+)();
